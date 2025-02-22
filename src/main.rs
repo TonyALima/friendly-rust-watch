28514986a77a7a4c3d1c5149a10ea88ff6b60f1d
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2025-02-22 13:59:31 -0300

    First prototype read temperature and humidity I2C sensor and show information in I2C OLED display

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
-//! Blinks an LED
-//!
-//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
-//!
-//! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
-//! the reference manual for an explanation. This is not an issue on the blue pill.
-
 #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-
-use nb::block;
-
+use aht10;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use heapless::String;
+use panic_reset as _;
+use shared_bus::BusManagerSimple;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use stm32f1xx_hal::{
+    i2c::{DutyCycle, I2c, Mode},
+    pac,
+    prelude::*,
+    timer::FTimer,
+};
+
+macro_rules! debug {
+    ($($x:tt)*) => {
+        {
+            #[cfg(debug_assertions)]
+            {
+                cortex_m_semihosting::hprintln!($($x)*)
+            }
+            #[cfg(not(debug_assertions))]
+            {
+
+            }
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
+    // let cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
@@ -27,26 +47,92 @@ fn main() -> ! {
     // HAL structs
     let mut flash = dp.FLASH.constrain();
     let rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain();
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.MHz())
+        .sysclk(48.MHz())
+        .pclk1(24.MHz())
+        .freeze(&mut flash.acr);
 
     // Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split();
+    let mut gpiob = dp.GPIOB.split();
 
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
+    let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+    let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
 
-    // Wait for the timer to trigger an update and change the state of the LED
+    let i2c_dev = I2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400.kHz(),
+            duty_cycle: DutyCycle::Ratio16to9,
+        },
+        clocks,
+    )
+    .blocking_default(clocks);
+
+    let bus = BusManagerSimple::new(i2c_dev);
+
+    let interface = I2CDisplayInterface::new(bus.acquire_i2c());
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    display.clear(BinaryColor::Off).unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    display.flush().unwrap();
+
+    let tim2: FTimer<pac::TIM2, 1_000> = FTimer::new(dp.TIM2, &clocks);
+    let mut tim2_delay = tim2.delay();
+
+    let mut aht10_dev =
+        aht10::Aht10::new(aht10::Address::Default, bus.acquire_i2c(), &mut tim2_delay).unwrap();
+
     loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+        display.clear(BinaryColor::Off).unwrap();
+        let (h, t) = aht10_dev.read(&mut tim2_delay).unwrap();
+        debug!("t: {}", t.raw());
+        debug!("h: {}", h.raw());
+        let t = float_to_string(t.celsius());
+        let h = float_to_string(h.rh());
+        let mut temperature_text: String<20> = String::new();
+        temperature_text.push_str("Temperature: ").unwrap();
+        temperature_text.push_str(&t).unwrap();
+        temperature_text.push_str("C").unwrap();
+        Text::with_baseline(&temperature_text, Point::zero(), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        let mut humidity_text: String<16> = String::new();
+        humidity_text.push_str("Humidity: ").unwrap();
+        humidity_text.push_str(&h).unwrap();
+        humidity_text.push_str("%").unwrap();
+        Text::with_baseline(&humidity_text, Point::new(0, 16), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        display.flush().unwrap();
+
+        tim2_delay.delay_ms(5_000u16);
     }
+}
+
+fn float_to_string(f: f32) -> String<4> {
+    let mut s: String<4> = String::new();
+    s.push((((f / 10.0) as u8) % 10 + 48) as char).unwrap();
+    s.push((((f / 1.0) as u8) % 10 + 48) as char).unwrap();
+    s.push('.').unwrap();
+    s.push((((((f / 0.1) as u16) % 10) as u8) + 48) as char)
+        .unwrap();
+    s
 }
